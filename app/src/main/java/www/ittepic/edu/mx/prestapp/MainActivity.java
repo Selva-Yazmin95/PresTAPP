47028cdbe732f83e127b7039e8b58e356670002c
AbrilHernandez <abyahernandezsa@ittepic.edu.mx>
2016-05-26 08:04:59 -0600

    Initial commit

---
+++ app/src/main/java/www/ittepic/edu/mx/prestapp/MainActivity.java
@@ -0,0 +1,13 @@
+package www.ittepic.edu.mx.prestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
