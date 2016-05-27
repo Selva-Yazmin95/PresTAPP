6e88c7f01bba677a28fb964a194fe1d2caaafe00
LuzGonzalez <luzalgonzalezro@ittepic.edu.mx>
2016-05-26 21:07:03 -0600

    Main y activity_main

---
+++ Main.java
@@ -0,0 +1,22 @@
+package www.ittepic.edu.mx.prestapp;
+
+import android.content.Intent;
+import android.support.uv7.app.ActionBarActivity;
+import android.support.uv7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import www.ittepic.edu.mx.prestapp.R;
+
+public class Main extends ActionBarActivity{
+
+    @Override
+    protected void onCreate(Bundle.saveInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}//main
